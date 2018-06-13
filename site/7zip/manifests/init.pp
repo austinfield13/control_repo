f7316aeb68d0737ee82ed1b9086451124e8b9b08
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-13 10:49:02 -0600

    Update init.pp

--- site/7zip/manifests/init.pp
+++ site/7zip/manifests/init.pp
@@ -2,5 +2,5 @@ class 7zip {
   package {'7zip':
   ensure          => present,
   source          => 'S:\7zip\7z1805-x64.exe',
-  install_options => ['/S', {'INSTALLDIR' => 'C:\opt\7zip'}],}
+  install_options => [ {'INSTALLDIR' => 'C:\opt\7zip'}],}
 }
