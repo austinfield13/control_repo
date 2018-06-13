ba2a08ef8bc0d7eb54e692902a96d63024d26a48
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-13 08:22:12 -0600

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,8 +2,4 @@ class profile::base {
   user {'default':
     ensure => present,}
     
-  package {'7zip':
-    ensure          => installed,
-    source          => 'S:\7zip\7z1805-x64.exe',
-    install_options => ['/S', {'INSTALLDIR' => 'C:\opt\7zip'}],}
 }
