6378b0a1396fe320a2aa0ac98d3f1ac1d2390667
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-13 11:50:30 -0600

    Update setup_7zip.pp

--- site/profile/manifests/setup_7zip.pp
+++ site/profile/manifests/setup_7zip.pp
@@ -1,5 +1,5 @@
 class profile::setup_7zip {
   package {'7zip':
     ensure          => present,
-    source          => 'S:\7zip\7z1805-x64.exe',}
+    source          => 'https://www.7-zip.org/download.html',}
 }
