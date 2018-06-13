bbb45a4937d1ffc5cf3f0cd026def28cbf04eebe
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-13 10:29:55 -0600

    Update setup_7zip.pp

--- site/profile/manifests/setup_7zip.pp
+++ site/profile/manifests/setup_7zip.pp
@@ -2,5 +2,5 @@ class profile::setup_7zip {
   package {'7zip':
   ensure          => installed,
   source          => 'S:\7zip\7z1805-x64.exe',
-  install_options => [{'INSTALLDIR' => 'C:\opt\7zip'}],}
+  }
 }
