882ea7bc211599602c29b054aec0f4885229bc77
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-15 11:29:41 -0600

    Update setup_eclipse.pp

--- site/profile/manifests/setup_eclipse.pp
+++ site/profile/manifests/setup_eclipse.pp
@@ -1,6 +1,12 @@
 class profile::setup_eclipse{
+  exec {'mkdir':
+    before     => File['C:/opt/eclipse'],
+    command    => "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command mkdir 'C:/opt/eclipse'",
+  }
+  
   file {'C:/opt/eclipse':
     before     => Exec['unzip'],
+    require    => Exec['mkdir'],
     ensure     => present,
     source     =>'S:/eclipse/eclipse-jee-oxygen-3a-win32-x86_64.zip',
   }
