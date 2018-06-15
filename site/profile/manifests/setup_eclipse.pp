572e1f2adb1aacec6048f457768a8fd83d3d79e9
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-15 13:01:08 -0600

    Update setup_eclipse.pp

--- site/profile/manifests/setup_eclipse.pp
+++ site/profile/manifests/setup_eclipse.pp
@@ -10,7 +10,7 @@ class profile::setup_eclipse{
     command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
   }
   
-  file {'C:\opt\eclipse\eclipse.zip':
+  file {'C:\opt\eclipse\eclipse-delete.zip':
     require    => Exec['unzip'],
     ensure     => absent,
     source     => 'C:\opt\eclipse\eclipse.zip',
