14d1ca67198b7eb337ec3569e1f7b666a9556158
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-15 15:27:45 -0600

    Update setup_7zip.pp

--- site/profile/manifests/setup_7zip.pp
+++ site/profile/manifests/setup_7zip.pp
@@ -1,6 +1,6 @@
 class profile::setup_7zip {
   exec {'7zip':
     path       => 'S:/7zip/7z1805-x64.exe',
-    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/7zip/7z1805-x64.exe'  -ArgumentList '/S /D='C:\opt\7zip\''", 
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/7zip/7z1805-x64.exe'  -ArgumentList '/S /D=C:\opt\7zip\'", 
   }
 }
