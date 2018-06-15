6b78648d611a0309778084fe6bcf225714eb384f
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-15 11:41:28 -0600

    Update setup_eclipse.pp

--- site/profile/manifests/setup_eclipse.pp
+++ site/profile/manifests/setup_eclipse.pp
@@ -1,18 +1,12 @@
 class profile::setup_eclipse{
-  exec {'mkdir':
-    before     => File['C:/opt/eclipse'],
-    command    => "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command mkdir 'C:/opt/eclipse'",
-  }
-  
-  file {'C:/opt/eclipse':
+  file {'C:/opt/eclipse/eclipse.zip':
     before     => Exec['unzip'],
-    require    => Exec['mkdir'],
-    ensure     => directory,
+    ensure     => present,
     source     =>'S:/eclipse/eclipse-jee-oxygen-3a-win32-x86_64.zip',
   }
 
   exec {'unzip':
-    require    => File['C:/opt/eclipse'],
-    command    =>"C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command Expand-Archive -Path 'C:/opt/eclipse/eclipse-jee-oxygen-3a-win32-x86_64.zip' -DestinationPath 'C:/opt/eclipse'",
+    require    => File['C:/opt/eclipse/eclipse.zip'],
+    command    =>"C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command Expand-Archive -Path 'C:/opt/eclipse/eclipse.zip' -DestinationPath 'C:/opt/eclipse'",
   }
 }
