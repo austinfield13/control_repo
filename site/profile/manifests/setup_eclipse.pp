32e65006352d6d03239093571c8a6202606b72cc
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-15 12:33:51 -0600

    Update setup_eclipse.pp

--- site/profile/manifests/setup_eclipse.pp
+++ site/profile/manifests/setup_eclipse.pp
@@ -1,12 +1,12 @@
 class profile::setup_eclipse{
-  file {'C:\opt\eclipse\eclipse.zip':
+  file {'C:\opt\eclipse\README.txt':
     before     => Exec['unzip'],
     ensure     => present,
-    source     => 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip',
+    source     => 'S:\eclipse\README.txt',
   }
 
-  exec {'unzip':
-    require    => File['C:\opt\eclipse\eclipse.zip'],
-    command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
-  }
+#  exec {'unzip':
+#    require    => File['C:\opt\eclipse\eclipse.zip'],
+#    command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
+#  }
 }
