82e25e68a84484507b297b4e1a3db0dd570cb80a
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 08:22:25 -0600

    Update setup_ads.pp

--- site/profile/manifests/setup_ads.pp
+++ site/profile/manifests/setup_ads.pp
@@ -2,8 +2,8 @@ class profile::setup_ads {
 
   exec{'ApacheDirectoryStudio':
 
-  path       => 'S:/ads/ApacheDirectoryStudio-2.0.0v20170904-M13-win32.win32.x86_64.exe'
-  command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/ads/ApacheDirectoryStudio-2.0.0v20170904-M13-win32.win32.x86_64.exe'  -ArgumentList '/S /D=C:\opt\ads\'", 
+    path       => 'S:/ads/ApacheDirectoryStudio-2.0.0v20170904-M13-win32.win32.x86_64.exe'
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/ads/ApacheDirectoryStudio-2.0.0v20170904-M13-win32.win32.x86_64.exe'  -ArgumentList '/S /D=C:\opt\ads\'", 
 
   }
 }
