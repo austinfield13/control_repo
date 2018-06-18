d005286ed4f9667ba5e35d085a29bd8e1441f04a
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 11:52:14 -0600

    Update init.pp

--- site/setup_atom/manifests/init.pp
+++ site/setup_atom/manifests/init.pp
@@ -1,1 +1,31 @@
+class setup_atom {
 
+  file {'C:\opt\atom':
+
+    before     => File['C:\opt\atom\atom.zip'],
+    ensure     => directory,
+
+  }
+
+  file {'C:\opt\atom\atom.zip':
+
+    before     => Exec['unzip'],
+    ensure     => present,
+    source     => 'S:\atom\atom-x64-windows.zip',
+
+  }
+
+  exec {'unzip':
+
+    require    => File['C:\opt\atom\atom.zip'],
+    command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\atom\atom.zip' -DestinationPath 'C:\opt\atom'",
+
+  }
+
+  exec {'remove file':
+
+    require    => Exec['unzip'],
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command rm C:\opt\atom\atom.zip",
+
+  }
+}
