d97d13e0b0bdcccaf95aef8622cb70a207110c5d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 11:44:21 -0600

    Create init.pp

---
+++ site/setup_eclipse/manifests/init.pp
@@ -0,0 +1,31 @@
+class setup_eclipse {
+
+  file {'C:\opt\eclipse':
+
+    before     => File['C:\opt\eclipse\eclipse.zip'],
+    ensure     => directory,
+
+  }
+
+  file {'C:\opt\eclipse\eclipse.zip':
+
+    before     => Exec['unzip'],
+    ensure     => present,
+    source     => 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip',
+
+  }
+
+  exec {'unzip':
+
+    require    => File['C:\opt\eclipse\eclipse.zip'],
+    command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
+
+  }
+
+  exec {'remove file':
+
+    require    => Exec['unzip'],
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command rm C:\opt\eclipse\eclipse.zip",
+
+  }
+}
