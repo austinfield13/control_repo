8de618f87721beb2cdd6986165ce9e74d3bd1128
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 11:57:45 -0600

    Update init.pp

--- site/setup_jdk/manifests/init.pp
+++ site/setup_jdk/manifests/init.pp
@@ -1,3 +1,11 @@
 class setup_jdk {
 
+  exec {'jdk':
+
+    path       => 'S:/java/jdk-8u162-windows-x64.exe',
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/java/jdk-8u162-windows-x64.exe'  -ArgumentList '/S /D=C:\opt\jdk\'", 
+
+  }
 }
+
+
