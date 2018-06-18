5bb3bbf6dd81b4cf570f4555ca99fbebcb120578
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 13:10:08 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,11 @@
 node default {
   include setup_jdk
-  include setup_python
+  #include setup_python
 }
 node 'alcor' inherits default {
   include setup_eclipse
   include setup_7zip
   include setup_atom
+  include setup_ads
 }
 
