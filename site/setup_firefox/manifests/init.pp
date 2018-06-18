0e8b838be2d2a3c28d680b7ce0d6e7013bb9249e
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 16:05:56 -0600

    Create init.pp

---
+++ site/setup_firefox/manifests/init.pp
@@ -0,0 +1,11 @@
+class setup_firefox {
+
+  package {'firefox':
+  
+    ensure           => installed,
+	  source           => 'S:/firefox/Firefox Setup 60.0.1.exe',
+	  install_options  => [/InstallDirectoryPath = 'C:\opt\firefox'],
+    
+  }
+
+}
