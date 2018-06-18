2bc963f0bbfd7fe090a4016a1e6d49c22712d30b
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 16:08:05 -0600

    Update init.pp

--- site/setup_firefox/manifests/init.pp
+++ site/setup_firefox/manifests/init.pp
@@ -3,8 +3,8 @@ class setup_firefox {
   package {'firefox':
   
     ensure           => installed,
-	  source           => 'S:/firefox/Firefox Setup 60.0.1.exe',
-	  install_options  => [/InstallDirectoryPath = 'C:\opt\firefox'],
+    source           => 'S:/firefox/Firefox Setup 60.0.1.exe',
+    install_options  => ['/InstallDirectoryPath' = 'C:\opt\firefox'],
     
   }
 
