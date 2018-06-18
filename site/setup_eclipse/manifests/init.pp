c85787b64df40140e0f2535a6970c0214618d052
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 14:34:58 -0600

    Update init.pp

--- site/setup_eclipse/manifests/init.pp
+++ site/setup_eclipse/manifests/init.pp
@@ -2,23 +2,34 @@ class setup_eclipse {
 
   file {'C:\opt\eclipse':
 
-    before     => File['C:\opt\eclipse\eclipse.zip'],
-    ensure     => directory,
+    before      => File['C:\opt\eclipse\eclipse.zip'],
+    ensure      => directory,
 	
   }
 
   file {'C:\opt\eclipse\eclipse.zip':
 
-    require    => File['C:\opt\eclipse'],
-    ensure     => present,
-    source     => 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip',
-
+    require     => File['C:\opt\eclipse'],
+    ensure      => present,
+    source      => 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip',
+   
   }
 
   exec {'unzip_eclipse':
 
-    require    => File['C:\opt\eclipse\eclipse.zip'],
-    command    =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
-    
+    subscribe   => File['C:\opt\eclipse\eclipse.zip'],
+    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
+    refreshonly => true,
+  
   }
+  
+  exec {'remove eclipse.zip':
+
+    subscribe   => Exec['unzip_eclipse'],
+    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command rm C:\opt\eclipse\eclipse.zip",
+    refreshonly => true,
+ 
+  }
+  
 }
+
