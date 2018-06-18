cc138043e8024a93c0376913a55b192007839f23
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 12:43:16 -0600

    Update init.pp

--- site/setup_jdk/manifests/init.pp
+++ site/setup_jdk/manifests/init.pp
@@ -2,11 +2,11 @@ class setup_jdk {
 
   file {'C:\opt\java':
   
-    ensure     => present,
+    ensure     => directory,
     
   exec {'jdk_install':
 
-    requires   => File ['C:\opt\java'],
+    require    => File ['C:\opt\java'],
     path       => 'S:/java/jdk-8u162-windows-x64.exe',
     command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/java/jdk-8u162-windows-x64.exe'  
                    -ArgumentList '/S ADDLOCAL=`"ToolsFeature,PublicjreFeature`" /INSTALLDIRPUBJRE=C:\opt\java\jre8 INSTALLDIR=C:\opt\java'", 
