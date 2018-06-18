7bf82a30d29e16395303f9b9d780e570ff2c0259
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 15:15:35 -0600

    Update init.pp

--- site/setup_java/manifests/init.pp
+++ site/setup_java/manifests/init.pp
@@ -9,8 +9,8 @@ class setup_java {
   package {'java':
   
     ensure          => installed,
-	  source          => 'S:/java/jdk-8u162-windows-x64.exe',
-	  install_options => ['/s', {ADDLOCAL=`'ToolsFeature,PublicjreFeature1'`}, {'INSTALLDIRPUBJRE' => 'C:\opt\java\jre8'}, {'INSTALLDIR' => 'C:\opt\java'}],
+    source          => 'S:/java/jdk-8u162-windows-x64.exe',
+    install_options => ['/s', {ADDLOCAL=`'ToolsFeature,PublicjreFeature1'`}, {'INSTALLDIRPUBJRE' => 'C:\opt\java\jre8'}, {'INSTALLDIR' => 'C:\opt\java'}],
 
   }
 }
