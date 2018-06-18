ee0de9e55ea32daf2f916dc8e48f086f718d30c0
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-18 11:41:36 -0600

    Update init.pp

--- site/setup_7zip/manifests/init.pp
+++ site/setup_7zip/manifests/init.pp
@@ -1,1 +1,9 @@
-class 
+class setup_7zip{
+
+  exec {'7zip':
+
+    path       => 'S:/7zipp/7z1805-x64.exe',
+    command    => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/7zip/7z1805-x64.exe'  -ArgumentList '/S /D=C:\opt\7zipp\'", 
+
+  }
+}
