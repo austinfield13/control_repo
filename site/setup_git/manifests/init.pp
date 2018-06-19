3167bd779ee5522b8d7fc0525a3fbd98b75659a7
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 13:10:32 -0600

    Update init.pp

--- site/setup_git/manifests/init.pp
+++ site/setup_git/manifests/init.pp
@@ -1,6 +1,17 @@
 class setup_git {
 
+  file {'C:\opt\git':
 
+    ensure     => directory,
 
+  }
 
+  package {'git':
+
+    ensure          => installed,
+    source          => 'S:/git/Git-2.16.2-64-bit.exe',
+    install_options => [{'/INI' => 'S:\git\git-install.ini'}],
+
+  }
 }
+
