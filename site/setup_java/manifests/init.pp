5379527c73ae83588591dffda42d2df6fa65c37e
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 13:01:53 -0600

    Update init.pp

--- site/setup_java/manifests/init.pp
+++ site/setup_java/manifests/init.pp
@@ -10,7 +10,7 @@ class setup_java {
   
     ensure          => installed,
     source          => 'S:/java/jdk-8u162-windows-x64.exe',
-    install_options => [{'/cfg' => 'S:\java\java-install.cfg'}],
+    install_options => [{'INSTALLCFG' => 'S:\java\java-install.cfg'}],
 
   }
 }
