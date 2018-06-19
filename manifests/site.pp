3c5e39be7fee37a8c9860663150dae33c31d0de4
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 14:29:34 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,15 +3,16 @@ node default {
   #include setup_python
 }
 node 'alcor' {
-  include setup_java
-  include setup_eclipse
-  include setup_7zip
-  include setup_atom
-  include setup_ads
+  #include setup_java
+  #include setup_eclipse
+  #include setup_7zip
+  #include setup_atom
+  #include setup_ads
   #include setup_docker
-  include setup_firefox
-  include setup_git
+  #include setup_firefox
+  #include setup_git
   #include setup_gpg
-  include setup_hipchat
+  #include setup_hipchat
+  include setup_hyperv
 }
 
