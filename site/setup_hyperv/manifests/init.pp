23d381ffa9b3572247b22d91a252654da4d7043d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 14:28:45 -0600

    Update init.pp

--- site/setup_hyperv/manifests/init.pp
+++ site/setup_hyperv/manifests/init.pp
@@ -1,1 +1,8 @@
+class setup_hyperv {
 
+  exec {'hyperv':
+  
+    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All'
+	
+  }
+}
