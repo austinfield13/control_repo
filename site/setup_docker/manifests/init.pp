834b05bac1aad86bbc0cfaa3a06a9476fe639f17
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 10:44:04 -0600

    Update init.pp

--- site/setup_docker/manifests/init.pp
+++ site/setup_docker/manifests/init.pp
@@ -1,10 +1,15 @@
 class setup_docker {
 
+  file {'C:\opt\docker':
+    
+	  ensure            => directory,
+	
+  }
+
   package {'docker':
     ensure            => installed,
     source            => 'S:\docker\Docker for Windows Installer.exe',
     install_options   => ['/S', {'INSTALLDIR' => 'C:\opt\docker'}],
-    
+	
   }
-    
 }
