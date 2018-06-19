fb7717ad73ab50edd0bf42cb64ba28e20ee1638c
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 15:02:36 -0600

    Create init.pp

---
+++ site/setup_jetbrains/manifests/init.pp
@@ -0,0 +1,30 @@
+class setup_jetbrains {
+
+  file {'C:\opt\pycharm':
+  
+    ensure          => directory,
+	
+  }
+
+  package {'pycharm':
+  
+    ensure          => installed,
+	  source          => 'S:\jetbrains\pycharm-community-2018.1.1.exe',
+	  install_options => ['/S', {'/CONFIG' => 'S:\jetbrains\pycharm.silent.config'}, {'/D' => 'C:\opt\pycharm'}],
+	
+  }
+
+  file {'C:\opt\intellij':
+  
+    ensure          => directory,
+	
+  }
+
+  package {'intellij':
+  
+    ensure          => installed,
+	  source          => 'S:\jetbrains\ideaIC-2018.1.1.exe',
+	  install_options => ['/S', {'/CONFIG' => 'S:\jetbrains\intellij.silent.config'}, {'/D' => 'C:\opt\intellij'}],
+	
+  }
+}
