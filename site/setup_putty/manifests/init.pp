9fe8817f27d1efaab8d33d126f5e6817e6cb260a
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 16:08:11 -0600

    Create init.pp

---
+++ site/setup_putty/manifests/init.pp
@@ -0,0 +1,16 @@
+class setup_putty {
+
+  file {'C:\opt\putty':
+
+    ensure          => directory,
+
+  }
+
+  package {'putty':
+
+    ensure          => installed,
+    source          => 'S:\putty\putty-64bit-0.70-installer.msi',
+    install_options => ['/qn', {'/INSTALLDIR' => 'C:\opt\putty'}],
+
+  }
+}
