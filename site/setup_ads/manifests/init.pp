76d5ad0102f4bb3a5fbb1e16628ca839ad2caff6
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-19 11:23:28 -0600

    Update init.pp

--- site/setup_ads/manifests/init.pp
+++ site/setup_ads/manifests/init.pp
@@ -6,12 +6,18 @@ class setup_ads {
 	
   }
 
+  file {'C:\opt\ads\ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe':
+  
+    ensure      => present,
+    source      => 'S:/ads/ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe',
+	
+  }
+
   exec{'ApacheDirectoryStudio':
 
     subscribe   => File['C:\opt\ads'],
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'S:/ads/ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe'  -ArgumentList '/S /D=C:\opt\ads\'", 
+    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'C:/opt/ads/ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe'  -ArgumentList '/S /D=C:\opt\ads\'", 
     refreshonly => true,
     
   }
 }
- 
