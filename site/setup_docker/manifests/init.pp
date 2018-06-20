7c8dcf23f7a9a1a9f74f348d587ab8f7d8509b3b
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-20 11:46:41 -0600

    Update init.pp

--- site/setup_docker/manifests/init.pp
+++ site/setup_docker/manifests/init.pp
@@ -10,7 +10,7 @@ class setup_docker {
   
     ensure            => installed,
     source            => 'S:\docker\Docker for Windows Installer.exe',
-    install_options   => ['/SP-', 'VERYSILENT', {'/DIR' => 'C:\opt\docker'}],
+    install_options   => ['VERYSILENT', {'/DIR' => 'C:\opt\docker'}],
 	
   }
 }
