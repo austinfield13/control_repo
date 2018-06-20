2adf24ac5b387d9fe79315403fea8cd59101d0cd
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-20 09:29:10 -0600

    Update init.pp

--- site/setup_hyperv/manifests/init.pp
+++ site/setup_hyperv/manifests/init.pp
@@ -2,7 +2,7 @@ class setup_hyperv {
 
   exec {'hyperv':
   
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'DISM /Online /Enable-Feature /All /FeatureName: Microsoft-Hyper-V'",
+    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'DISM /Online /Enable-Feature /All /FeatureName: Microsoft-Hyper-V -wait'",
 	
   }
 }
