6f791bca493d090ad707ff38764acba8f0476d62
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-20 08:42:23 -0600

    Create init.pp

---
+++ site/setup_w10_offnet_sophosdx/manifests/init.pp
@@ -0,0 +1,24 @@
+class setup_w10_offnet_sophosdx {
+
+  file {'C:\opt\W10-OffNet-SophosDX':
+  
+    ensure          => directory,
+	
+  }
+
+  package {'Sophosdx':
+  
+    ensure          => present,
+    source          => 'S:\W10-OffNet-SophosDX\SophosDE.exe'
+    install_options => ['/S', {'/DIR' => 'C:\opt\W10-OffNet-SophosDX'}],
+
+  }
+
+  package {'Sophos-Standalone':
+  
+    ensure          => present,
+    source          => 'S:\W10-OffNet-SophosDX\NG_Standalone_DX.msi'
+    install_options => ['/S', {'/DIR' => 'C:\opt\W10-OffNet-SophosDX'}],
+	
+  }
+}
