e1a693349ca80294d14f4034104606058e985f05
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-20 12:40:52 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,8 +7,8 @@ node 'alcor' {
   #include setup_eclipse
   #include setup_7zip
   #include setup_atom
-  #include setup_ads
-  include setup_docker
+  include setup_ads
+  #include setup_docker
   #include setup_firefox
   #include setup_git
   #include setup_gpg
