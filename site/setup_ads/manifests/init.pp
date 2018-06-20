5bff248b42f83dd861ff3a8c25125da7a342c144
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-20 12:40:17 -0600

    Update init.pp

--- site/setup_ads/manifests/init.pp
+++ site/setup_ads/manifests/init.pp
@@ -13,11 +13,12 @@ class setup_ads {
 	
   }
 
-  exec{'ApacheDirectoryStudio':
-
-    subscribe   => File['C:\opt\ads'],
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Start-Process -Wait 'C:/opt/ads/ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe'  -ArgumentList '/S /D=C:\opt\ads\'", 
-    refreshonly => true,
-    
+  package {'ApacheDirectoryStudio':
+  
+    ensure          => installed,
+    source          => 'C:\opt\ads\ApacheDirectoryStudio-2.0.0.v20170904-M13-win32.win32.x86_64.exe',
+    install_options => ['/S', '/D=C:\opt\ads'],
+	
   }
 }
+
