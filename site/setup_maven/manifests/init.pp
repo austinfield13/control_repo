3fcdcde7d23eae4b76065312d364c0b70a582e83
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 08:43:49 -0600

    Update init.pp

--- site/setup_maven/manifests/init.pp
+++ site/setup_maven/manifests/init.pp
@@ -6,28 +6,11 @@ class setup_maven {
 	
   }
   
-  file {'C:\opt\maven\maven.zip':
-
-    require     => File['C:\opt\maven'],
-    ensure      => present,
-    source      => 'S:\maven\apache-maven-3.5.3-bin.zip', 
-
-  }
-
   exec {'unzip_maven':
 
-    subscribe   => File['C:\opt\maven\maven.zip'],
-    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\maven\maven.zip' -DestinationPath 'C:\opt\maven'",
+    subscribe   => File['C:\opt\maven'],
+    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\maven\apache-maven-3.5.3-bin.zip' -DestinationPath 'C:\opt\maven'",
     refreshonly => true,
 
   }
- 
-  exec {'remove maven.zip':
-
-    subscribe   => Exec['unzip_maven'],
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command rm C:\opt\maven\maven.zip",
-    refreshonly => true,
-
-  }
-
 }
