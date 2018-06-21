4e49902199c22a08532415f09a1cad41910bcc81
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 08:26:35 -0600

    Update init.pp

--- site/setup_eclipse/manifests/init.pp
+++ site/setup_eclipse/manifests/init.pp
@@ -7,29 +7,10 @@ class setup_eclipse {
 	
   }
 
-  file {'C:\opt\eclipse\eclipse.zip':
-
-    require     => File['C:\opt\eclipse'],
-    ensure      => present,
-    source      => 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip',
-   
-  }
-
   exec {'unzip_eclipse':
 
     subscribe   => File['C:\opt\eclipse\eclipse.zip'],
-    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\opt\eclipse\eclipse.zip' -DestinationPath 'C:\opt\eclipse'",
+    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip' -DestinationPath 'C:\opt\eclipse'",
     refreshonly => true,
-  
-  }
-  
-  exec {'remove eclipse.zip':
-
-    subscribe   => Exec['unzip_eclipse'],
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command rm C:\opt\eclipse\eclipse.zip",
-    refreshonly => true,
- 
-  }
   
 }
-
