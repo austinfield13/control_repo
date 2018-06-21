aa62f94c6ce3d16974788da66b23f0aed99edec7
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 08:44:57 -0600

    Update init.pp

--- site/setup_maven/manifests/init.pp
+++ site/setup_maven/manifests/init.pp
@@ -9,7 +9,7 @@ class setup_maven {
   exec {'unzip_maven':
 
     subscribe   => File['C:\opt\maven'],
-    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'C:\maven\apache-maven-3.5.3-bin.zip' -DestinationPath 'C:\opt\maven'",
+    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'S:\maven\apache-maven-3.5.3-bin.zip' -DestinationPath 'C:\opt\maven'",
     refreshonly => true,
 
   }
