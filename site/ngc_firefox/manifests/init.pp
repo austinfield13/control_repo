a8fdf9c82c9e3cdfb4bee5b9c2a5300b2bcd5047
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 14:52:56 -0600

    Create init.pp

---
+++ site/ngc_firefox/manifests/init.pp
@@ -0,0 +1,17 @@
+class ngc_firefox {
+
+  file {'C:\opt\firefox':
+  
+    ensure           => directory,
+	
+  }
+
+  package {'firefox':
+
+    ensure           => installed,
+    source           => 'S:/firefox/Firefox Setup 60.0.1.exe',
+    install_options  => [{'/INI' => 'S:\firefox\firefox-install.ini'}],
+
+  }
+
+}
