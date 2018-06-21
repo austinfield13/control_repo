17e40a5b8fb6ba780c90e5162115ce9d176d479d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 16:11:17 -0600

    Create init.pp

---
+++ site/ngc_selenium/manifests/init.pp
@@ -0,0 +1,32 @@
+class ngc_selenium {
+
+  file {'C:\opt\selenium':
+  
+    ensure           => directory,
+	
+  }
+
+  file {'C:\opt\selenium\chrome':
+  
+    ensure           => directory,
+    source           => 'S:\selenium\chrome',
+    recurse          => remote,
+	
+  }
+
+  file {'C:\opt\selenium\firefox':
+  
+    ensure           => directory,
+    source           => 'S:\selenium\firefox',
+    recurse          => remote,
+	
+  }
+
+  file {'C:\opt\selenium\IE':
+  
+    ensure           => directory,
+    source           => 'S:\selenium\IE',
+    recurse          => remote,
+	
+  }
+}
