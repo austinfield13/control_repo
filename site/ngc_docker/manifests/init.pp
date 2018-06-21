c1c7d0a5bd0c709907ed02417ef9d90cc5ed77b9
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 14:48:38 -0600

    Create init.pp

---
+++ site/ngc_docker/manifests/init.pp
@@ -0,0 +1,16 @@
+class setup_docker {
+
+  file {'C:\opt\docker':
+    
+	  ensure            => directory,
+	
+  }
+
+  package {'docker':
+
+    ensure            => installed,
+    source            => 'S:\docker\Docker for Windows Installer.exe',
+    install_options   => [{'/DIR' => 'C:\opt\docker'}],
+	
+  }
+}
