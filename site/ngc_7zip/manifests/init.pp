d1efe8d73e97124640236678c75ef147ae661928
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 14:43:14 -0600

    Update init.pp

--- site/ngc_7zip/manifests/init.pp
+++ site/ngc_7zip/manifests/init.pp
@@ -1,1 +1,16 @@
+class setup_7zip{
 
+  file {'C:\opt\7zip':
+
+    ensure          => directory,
+
+  }
+
+  package {'7zipp':
+
+    ensure          => installed,
+    source          => 'S:\7zip\7z1805-x64.exe',
+    install_options => ['/S', '/D=C:\opt\7zipp'],
+
+  }
+} 
