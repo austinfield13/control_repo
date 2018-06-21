3b0ec3cadb1985352dd2c7e0084c9b201189a88d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 15:01:40 -0600

    Create init.pp

---
+++ site/ngc_java/manifests/init.pp
@@ -0,0 +1,17 @@
+class ngc_java {
+
+
+  file {'C:\opt\java':
+
+    ensure     => directory,
+
+  }
+
+  package {'java':
+
+    ensure          => installed,
+    source          => 'S:/java/jdk-8u162-windows-x64.exe',
+    install_options => [{'INSTALLCFG' => 'S:\java\java-install.cfg'}],
+
+  }
+}
