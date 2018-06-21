1dc5d04da5df0d2b22cc0a4800618e4d1f3af129
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-21 14:51:24 -0600

    Create init.pp

---
+++ site/ngc_eclipse/manifests/init.pp
@@ -0,0 +1,17 @@
+class ngc_eclipse {
+
+  file {'C:\opt\eclipse':
+
+    ensure      => directory,
+	
+  }
+
+  exec {'unzip_eclipse':
+
+    subscribe   => File['C:\opt\eclipse'],
+    command     =>"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command Expand-Archive -Path 'S:\eclipse\eclipse-jee-oxygen-3a-win32-x86_64.zip' -DestinationPath 'C:\opt\eclipse'",
+    refreshonly => true,
+
+  }
+  
+}
