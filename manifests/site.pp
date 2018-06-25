b1bb0a228c6f53c71c24fa9cff08275625e3b125
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-25 08:28:03 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,24 +2,7 @@ node default {
   include setup_jdk
   #include setup_python
 }
-node 'alcor' {
-  include ngc_java
-  include ngc_eclipse
+node 'alcor' { 
   include ngc_7zip
-  include ngc_atom
-  include ngc_ads
-  #include ngc_docker
-  include ngc_firefox
-  include ngc_git
-  include ngc_gpg
-  include ngc_hipchat
-  #include ngc_hyperv
-  include ngc_jetbrains
-  include ngc_kse
-  include ngc_maven
-  include ngc_pgadmin
-  include ngc_putty
-  include ngc_selenium
-  #include ngc_w10_offnet_sophosdx
 }
 
