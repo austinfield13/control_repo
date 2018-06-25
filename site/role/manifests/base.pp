b673a00947ed3a9c1b6a76d75d361a3d78980a8f
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-06-25 08:27:29 -0600

    Update base.pp

--- site/role/manifests/base.pp
+++ site/role/manifests/base.pp
@@ -1,5 +1,20 @@
 class role::base {
-  include profile::setup_eclipse
-  include profile::setup_7zip
-  include profile::setup_ads
+  include ngc_java
+  include ngc_eclipse
+  include ngc_7zip
+  include ngc_atom
+  include ngc_ads
+  include ngc_docker
+  include ngc_firefox
+  include ngc_git
+  include ngc_gpg
+  include ngc_hipchat
+  include ngc_hyperv
+  include ngc_jetbrains
+  include ngc_kse
+  include ngc_maven
+  include ngc_pgadmin
+  include ngc_putty
+  include ngc_selenium
+  include ngc_w10_offnet_sophosdx
 }
