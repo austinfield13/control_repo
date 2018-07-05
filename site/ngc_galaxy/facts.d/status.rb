7e11d7bf997657142bfd0687cfa12a0b47631b47
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-05 14:23:58 -0600

    Update status.rb

--- site/ngc_galaxy/facts.d/status.rb
+++ site/ngc_galaxy/facts.d/status.rb
@@ -1,4 +1,4 @@
-Facter.add(:status) do
+Facter.add('status') do
   setcode do
     Facter::Core::Execution.execute('Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object LicenseStatus')
   end 
