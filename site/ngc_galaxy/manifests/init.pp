0c08628228d72806365703de72207901427d8295
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-06 10:17:36 -0600

    Update init.pp

--- site/ngc_galaxy/manifests/init.pp
+++ site/ngc_galaxy/manifests/init.pp
@@ -1,69 +1,59 @@
 class ngc_galaxy {
 
-  #exec {'Checking the Status':
-  
-    #command  => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object LicenseStatus'"
-  #}
-
-  #$status = getparam(Exec["Checking the Status"],"command")
-
-  unless $status == 1  {
-  
     notice("Activating Windows operating system now")
 
-    exec {'Activate Windows Part 1':
+  exec {'Activate Windows Part 1':
 	  
-	  command    => "slmgr.vbs /skms 10.129.17.5:1688",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Windows Part 2':
-	
-	  command    => "slmgr.vbs /ato",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Windows Part 3':
-	
-	  command    => "slmgr.vbs /dli",
-	  provider   => powershell,
-	}
-
-	notice("Windows operating system activated")
-	notice("Activating Microsoft Office suite now")
-
-	exec {'Activate Office Part 1':
-	
-	  command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Office Part 2':
-	
-	  command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /sethst:10.129.17.5",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Office Part 3':
-	
-	  command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /act",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Office Part 4':
-	
-	  command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-	  provider   => powershell,
-	}
-
-	exec {'Activate Office Part 5':
-	
-	  command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /dstatus",
-	  provider   => powershell,
-	}
-
-	notice("Microsoft Office activated")
-
+	command    => "slmgr.vbs /skms 10.129.17.5:1688",
+	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
+	provider   => powershell,
   }
+
+  exec {'Activate Windows Part 2':
+	
+	command    => "slmgr.vbs /ato",
+	provider   => powershell,
+  }
+
+  exec {'Activate Windows Part 3':
+	
+	command    => "slmgr.vbs /dli",
+	provider   => powershell,
+  }
+
+  notice("Windows operating system activated")
+  notice("Activating Microsoft Office suite now")
+
+  exec {'Activate Office Part 1':
+	
+	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
+	provider   => powershell,
+  }
+
+  exec {'Activate Office Part 2':
+	
+	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /sethst:10.129.17.5",
+	provider   => powershell,
+  }
+
+  exec {'Activate Office Part 3':
+	
+	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /act",
+	provider   => powershell,
+  }
+
+  exec {'Activate Office Part 4':
+	
+	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
+	provider   => powershell,
+  }
+
+  exec {'Activate Office Part 5':
+	
+	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /dstatus",
+	provider   => powershell,
+  }
+
+  notice("Microsoft Office activated")
 
 }
