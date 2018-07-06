498ec719b0cc574feba8683b4c2e612c7fecd63f
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-06 09:14:04 -0600

    Update status.rb

--- site/ngc_galaxy/facts.d/status.rb
+++ site/ngc_galaxy/facts.d/status.rb
@@ -1,5 +1,5 @@
 Facter.add('status') do
   setcode do
-    Facter::Core::Execution.execute('Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object LicenseStatus')
+    Facter::Core::Execution.execute('if (Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property "LicenseStatus = 1") {exit 1}')
   end 
 end
