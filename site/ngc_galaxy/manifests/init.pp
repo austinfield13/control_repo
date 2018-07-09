b9161cb5043f92549b0df3aece92595761298b68
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-09 15:41:42 -0600

    Update init.pp

--- site/ngc_galaxy/manifests/init.pp
+++ site/ngc_galaxy/manifests/init.pp
@@ -1,66 +1,33 @@
 class ngc_galaxy {
 
-    notice("Activating Windows operating system now")
+  notice("Activating Windows operating system now")
 
+  exec {'Check Status':
+	
+    command     => $number = Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus| Where-Object{$_.LicenseStatus -ne '1'},
+    provider    => powershell,
+  }
+  
   exec {'Activate Windows Part 1':
-	  
-	command    => "slmgr.vbs /skms 10.129.17.5:1688",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
+
+    command     => "slmgr.vbs /skms 10.129.17.5:1688",
+    unless      => $number == nil,
+    provider    => powershell,
   }
 
   exec {'Activate Windows Part 2':
-	
-    command    => "slmgr.vbs /ato",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
+
+    subscribe   => Exec['Activate Windows Part 1'],
+    command     => "slmgr.vbs /ato",
+    provider    => powershell,
+    refreshonly => true,
   }
 
   exec {'Activate Windows Part 3':
-	
-	command    => "slmgr.vbs /dli",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
+
+    subscribe   => Exec['Activate Windows Part 2'],
+    command     => "slmgr.vbs /dli",
+    provider    => powershell,
+    refreshonly => true,
   }
-
-  notice("Windows operating system activated")
-  notice("Activating Microsoft Office suite now")
-
-  exec {'Activate Office Part 1':
-	
-	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
-  }
-
-  exec {'Activate Office Part 2':
-	
-	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /sethst:10.129.17.5",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
-  }
-
-  exec {'Activate Office Part 3':
-	
-	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /act",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
-  }
-
-  exec {'Activate Office Part 4':
-	
-	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
-  }
-
-  exec {'Activate Office Part 5':
-	
-	command    => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /dstatus",
-	unless     => "Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property 'LicenseStatus == 1'",
-	provider   => powershell,
-  }
-
-  notice("Microsoft Office activated")
-
 }
