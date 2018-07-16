57c345f85816fc41eff1b1aa1eef0e129637b445
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-16 10:32:27 -0600

    Update init.pp

--- site/ngc_galaxy/manifests/init.pp
+++ site/ngc_galaxy/manifests/init.pp
@@ -1,72 +1,9 @@
- class ngc_galaxy {
+class ngc_galaxy {
 
-  notice("Activating Windows operating system now")
-
-  exec {'Check Status':
-	
-    command     => "$number = Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus| Where-Object{$_.LicenseStatus -ne '1'}",
-    provider    => powershell,
-  }
+  exec {'Activate Windows and Office':
   
-  exec {'Activate Windows Part 1':
-
-    command     => "slmgr.vbs /skms 10.129.17.5:1688",
-    unless      => $number == nil,
-    provider    => powershell,
+    command    => 'S:\galaxy\Activate-GalaxyNet.ps1',
+    unless     => "if (Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus| Where-Object{$_.LicenseStatus -ne '1'}) {exit 1} else {exit 0}",
+    provider   => powershell, 
   }
-
-  exec {'Activate Windows Part 2':
-
-    command     => "slmgr.vbs /ato",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  exec {'Activate Windows Part 3':
-
-    command     => "slmgr.vbs /dli",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  notice("Windows operating system activated")
-  notice("Activating Microsoft Office suite now")
-
-  exec {'Activate Office Part 1':
-
-    command     => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  exec {'Activate Office Part 2':
-
-    command     => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /sethst:10.129.17.5",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  exec {'Activate Office Part 3':
-
-    command     => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /act",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  exec {'Activate Office Part 4':
-
-    command     => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /remhst",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  exec {'Activate Office Part 5':
-
-    command     => "cscript 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs' /dstatus",
-    unless      => $number == nil,
-    provider    => powershell,
-  }
-
-  notice("Microsoft Office activated")
-
 }
