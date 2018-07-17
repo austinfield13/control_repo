668455c227b8aa1e27e1854f4d9a748fb3a8cd84
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-17 13:16:37 -0600

    Update init.pp

--- site/ngc_galaxy/manifests/init.pp
+++ site/ngc_galaxy/manifests/init.pp
@@ -3,7 +3,7 @@ class ngc_galaxy {
   exec {'Activate Windows and Office':
   
     command    => 'powershell -executionpolicy remotesigned -file S:\galaxy\Activate-GalaxyNet.ps1',
-    unless     => "if ((Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus | Where-Object {$_.LicenseStatus -ne '1'}) -ne $null) {Exit '1'} else {Exit '0'}",
+    unless     => "if ((Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus | Where-Object {$_.LicenseStatus -ne '1'}) -ne $null) {Exit ("1")} else {Exit ("0")}",
     provider   => powershell,
     logoutput  => true,
   }
