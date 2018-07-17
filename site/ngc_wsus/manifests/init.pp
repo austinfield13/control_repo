7de3a0f46e94f5e26eb2d565dc3f64e3e8ef5e3d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-17 13:37:34 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -1,4 +1,11 @@
 class ngc_wsus {
 
+  exec{"Execute batch file":
+    
+    command      => "cd S:\galaxy\wsus | Start-Process wsus_mng_iso.bat",
+    onlyif       => "(Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name 'WUServer') -ne 'http://10.129.17.5'",
+    provider     => powershell,
+    
+  }
 
 }
