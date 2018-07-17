d08ff791ce09815295f77693415aec75275ea168
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-17 13:45:35 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -2,9 +2,10 @@ class ngc_wsus {
 
   exec{"Execute batch file":
     
-    command      => "cd S:\galaxy\wsus | Start-Process wsus_mng_iso.bat",
+    command      => 'powershell -executionpolicy remotesigned -file S:\galaxy\wsus\wsus_mng_iso.bat',
     onlyif       => "(Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name 'WUServer') -ne 'http://10.129.17.5'",
     provider     => powershell,
+    logoutput    => true,
     
   }
 
