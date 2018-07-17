9b0ab4ea4bf88a175f282971cc2e4707e923de77
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-17 11:18:27 -0600

    Update init.pp

--- site/ngc_galaxy/manifests/init.pp
+++ site/ngc_galaxy/manifests/init.pp
@@ -4,6 +4,7 @@ class ngc_galaxy {
   
     command    => 'S:\galaxy\Activate-GalaxyNet.ps1',
     unless     => "if (Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus| Where-Object{$_.LicenseStatus -ne '1'}) {exit 1} else {exit 0}",
-    provider   => powershell, 
+    provider   => powershell,
+    logoutput  => true,
   }
 }
