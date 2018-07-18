6768e454f65a4a6f31494957c0ca7f59316b54e1
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-18 08:25:20 -0600

    Create ngc_wsus_spec.rb

---
+++ site/ngc_wsus/spec/ngc_wsus_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+
+describe 'ngc_wsus' do
+
+  on_supported_os.each do |os, os_facts|
+
+    context "on #{os}" do
+
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+    end
+
+  end
+
+  describe 'Batch File Execution' do
+
+    it "should execute the batch file" do
+
+      is_expected.to contain_exec('Execute batch file').with('command' => "powershell -executionpolicy remotesigned -file S:\galaxy\wsus\wsus_mng_iso.bat", 'provider' => 'powershell', 'onlyif' => "(Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name 'WUServer') -ne 'http://10.129.17.5'"
+
+    end
+
+  end
+
+  describe 'resources' do
+
+    it 'should have one resource' do
+
+      is_expected.to have_resource_count(1)
+
+    end
+
+	  it 'should have one exec resource' do
+
+	    is_expected.to have_exec_resource_count(1)
+
+    end
+
+  end
+
+end
