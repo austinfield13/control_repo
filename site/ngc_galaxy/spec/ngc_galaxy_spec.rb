2203f6f79b7347b8a791fc2b05687df5e80c5a12
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-18 08:52:53 -0600

    Create ngc_galaxy_spec.rb

---
+++ site/ngc_galaxy/spec/ngc_galaxy_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+
+describe 'ngc_galaxy' do
+
+  on_supported_os.each do |os, os_facts|
+
+    context "on #{os}" do
+
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+    end
+
+  end
+
+  describe 'Activation Execution' do
+
+    it "should execute run the execution file" do
+
+      is_expected.to contain_exec('Activate Windows and Office').with('command' => 'powershell -executionpolicy remotesigned -file S:\galaxy\Activate-GalaxyNet.ps1', 'provider' => 'powershell', 'onlyif' => "(Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus | Where-Object {$_.LicenseStatus -ne '1'}) -ne $null"
+
+    end
+
+  end
+
+  describe 'resources' do
+
+    it 'should have one resource' do
+
+      is_expected.to have_resource_count(1)
+
+    end
+
+	  it 'should have one exec resource' do
+
+	    is_expected.to have_exec_resource_count(1)
+
+    end
+
+  end
+
+end
