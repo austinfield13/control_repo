53060cd46cf6a6d596b60e3e10d6304b2d89f232
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-18 13:54:52 -0600

    Update init.pp

--- site/ngc_hyperv/manifests/init.pp
+++ site/ngc_hyperv/manifests/init.pp
@@ -2,13 +2,14 @@ class ngc_hyperv {
 
   exec {'hyperv':
   
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V -wait'",
-	
+    command     => 'DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V',
+    provider    => 'powershell'
+    logoutput   => true,
   }
 
-  exec {'accept':
+  #exec {'accept':
   
-    command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'Y'",
+    #command     => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command 'Y'",
   
-  }
+  #}
 }
