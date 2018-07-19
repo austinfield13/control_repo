d29b2928c6cbf7b410493f68d36bad7b9e68f672
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-19 19:25:58 +0000

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -3,7 +3,7 @@ class ngc_wsus {
   exec{"Execute batch file":
     
     command      => "cd C:\opt\ | .\wsus.ps1",
-    onlyif       => "Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate | Select-Object -property WUServer | Where-Object -property WUServer -ne {Select-String -Pattern 'http://10.129.17.5'}",
+    onlyif       => "if((Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate | Select-Object -property WUServer | Where-Object -property WUServer) -eq $null){Exit 1}else{Exit 0}",
     provider     => 'powershell',
     logoutput    => true,
     
