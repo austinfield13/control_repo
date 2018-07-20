a6ecc8894b7639e5448cf8b42311e06568fc0158
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 13:32:21 -0600

    Update init.pp

--- site/ngc_ca/manifests/init.pp
+++ site/ngc_ca/manifests/init.pp
@@ -39,4 +39,76 @@ class ngc_ca {
     provider    => powershell,
     logoutput   => true,
   }
+  
+  exec {'ng-6.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-6.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "2CA0EC9BBE084E38F6BF08D130476E8279EEC142"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  #exec {'ng-7.cer':
+    #command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-7.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    #unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "2CA0EC9BBE084E38F6BF08D130476E8279EEC142"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    #returns     => 0,
+    #provider    => powershell,
+    #logoutput   => true,
+  #}
+  
+  exec {'ng-8.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-8.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "37BC452BA0F670FE18500E58819C963E610E0B5B"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-9.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-9.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "1A1CE71D4607E7A40DBE6AB35DFEDF7D7629CC01"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-10.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-10.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "EE2AA5E7ED1703EF983D02B49A590696496F5BDD"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-11.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-11.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "BCBA1909EE97704FBD5692C146CC1B79373D095D"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-12.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-12.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "2D3181AE0CD57A9F617E510FE4E0B45EAC471BCF"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-13.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-13.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "32DC0786EA565D90E1F9535DE8FE4D8E453F56BA"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+  
+  exec {'ng-14.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-14.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "DAB364C6A7282E54429C24D7FBD4F33F223075FF"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
 }
