7f1072c82cb3389c8ab389e52c2c7008ca83f17f
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 15:09:05 -0600

    Add files via upload

--- examples/ngc_wsus_spec.rb
+++ examples/ngc_wsus_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe 'ngc_wsus' do
 
-  on_supported_os.each do |os, os_facts|
+  =begin on_supported_os.each do |os, os_facts|
 
     context "on #{os}" do
 
@@ -14,12 +14,12 @@ describe 'ngc_wsus' do
     end
 
   end
+  =end
+  describe 'Powershell File Execution' do
 
-  describe 'Batch File Execution' do
+    it "should execute the wsus.ps1 file" do
 
-    it "should execute the batch file" do
-
-      is_expected.to contain_exec('Execute batch file').with('command' => "cd S:\galaxy\wsus | Start-Process wsus_mng_iso.bat", 'provider' => 'powershell', 'unless' => 'if ((Get-ItemProperty -Path hklm:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name "WUServer") -eq "http://10.129.17.5") {exit 0} else {exit 1})'
+      is_expected.to contain_exec('Execute WSUS Configuration').with('command' => '& C:\opt\wsus.ps1')
 
     end
 
