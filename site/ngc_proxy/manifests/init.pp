0bb5dc281d49d6ef27346adaae734e97bcf639a5
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 10:57:35 -0600

    Update init.pp

--- site/ngc_proxy/manifests/init.pp
+++ site/ngc_proxy/manifests/init.pp
@@ -1,9 +1,12 @@
-exec { 'winhttpproxy-1':
+class ngc_proxy {
+
+  exec { 'winhttpproxy-1':
   
-  command     => '&netsh winhttp set proxy "contractorproxywest.northgrum.com:80" "127.0.0.1,localhost,.northgrum.com,.entds.ngisn.com,10.*,172.16.*"',
-  unless      => 'if (((netsh winhttp show proxy) -like "*bypass list*" -split "+: +", 2)[1] -ne "127.0.0.1,localhost,.northgrum.com,.entds.ngisn.com,10.*,172.16.*") { Exit 1 }',
-  provider    => powershell,
-  returns     => 0,
-  logoutput   => true,
+    command     => '&netsh winhttp set proxy "contractorproxywest.northgrum.com:80" "127.0.0.1,localhost,.northgrum.com,.entds.ngisn.com,10.*,172.16.*"',
+    unless      => 'if (((netsh winhttp show proxy) -like "*bypass list*" -split "+: +", 2)[1] -ne "127.0.0.1,localhost,.northgrum.com,.entds.ngisn.com,10.*,172.16.*") { Exit 1 }',
+    provider    => powershell,
+    returns     => 0,
+    logoutput   => true,
   
-  }
+  } 
+}
