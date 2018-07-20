45980217805be7a4f9f2cacc7b595c22f20f4c44
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 13:02:11 -0600

    Create init.pp

---
+++ site/ngc_ca/manifests/init.pp
@@ -0,0 +1,10 @@
+class ngc_ca {
+
+  exec {'ng-1.cer':
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-1.cer" -CertStoreLocation "Cert:\\Trusted Root Certification Authorities\\Certificates"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\CurrentUser\\Root" | Where-Object {$_.Thumbprint -eq "FC772EBAC6A3B701764A6E4E8F09F06B0F726CF8"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    returns     => 0,
+    provider    => powershell,
+    logoutput   => true,
+  }
+}
