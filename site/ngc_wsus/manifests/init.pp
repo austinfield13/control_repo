20df6e14204f65555f5b6d30c78f2ff5522b6d76
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 15:02:24 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -1,6 +1,6 @@
 class ngc_wsus {
 
-  exec{"Execute batch file":
+  exec{"Execute WSUS Configuration":
     
     command      => '& C:\opt\wsus.ps1',
     unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ /f WUServer) -split "\s+",5)[5] -ne "http://10.129.17.5"){ Exit 1 }else{ Exit 0 }',
