159fdfd16d8cdada63fdd77a38601a2029fc14f2
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 13:07:28 -0600

    Update init.pp

--- site/ngc_ca/manifests/init.pp
+++ site/ngc_ca/manifests/init.pp
@@ -1,8 +1,8 @@
 class ngc_ca {
 
   exec {'ng-1.cer':
-    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-1.cer" -CertStoreLocation "Cert:\\CurrentUser\\Root"',
-    unless      => 'if((Get-ChildItem -Path "Cert:\\CurrentUser\\Root" | Where-Object {$_.Thumbprint -eq "FC772EBAC6A3B701764A6E4E8F09F06B0F726CF8"}) -eq $null){ Exit 1 } else { Exit 0 }',
+    command     => 'Import-Certificate -Filepath "S:\\ca\\certs\\ng-1.cer" -CertStoreLocation "Cert:\\LocalMachine\\Root"',
+    unless      => 'if((Get-ChildItem -Path "Cert:\\LocalMachine\\Root" | Where-Object {$_.Thumbprint -eq "FC772EBAC6A3B701764A6E4E8F09F06B0F726CF8"}) -eq $null){ Exit 1 } else { Exit 0 }',
     returns     => 0,
     provider    => powershell,
     logoutput   => true,
