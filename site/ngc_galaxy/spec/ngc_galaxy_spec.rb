ee9383f3c0f2761ff7bba067f7e45d97d80e94fb
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 15:17:58 -0600

    Add files via upload

--- site/ngc_galaxy/spec/ngc_galaxy_spec.rb
+++ site/ngc_galaxy/spec/ngc_galaxy_spec.rb
@@ -1,3 +1,4 @@
+
 require 'spec_helper'
 
 
@@ -15,11 +16,63 @@ describe 'ngc_galaxy' do
 
   end
 
-  describe 'Activation Execution' do
+  describe 'Windows Activation Executions' do
 
-    it "should execute run the execution file" do
+    it "should execute 'Check Status'" do
 
-      is_expected.to contain_exec('Activate Windows and Office').with('command' => 'powershell -executionpolicy remotesigned -file S:\galaxy\Activate-GalaxyNet.ps1', 'provider' => 'powershell', 'onlyif' => "(Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus | Where-Object -Property LicenseStatus -ne '1') -ne $null", 'logoutput' => true)
+      is_expected.to contain_exec('Check Status').with_command("$number = Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object PartialProductKey | Select-Object -property LicenseStatus| Where-Object{$_.LicenseStatus -ne '1'}")
+
+    end
+    
+    it "should execute 'Activate Windows Part 1'" do
+
+      is_expected.to contain_exec('Activate Windows Part 1').with('command' => "slmgr.vbs /skms 10.129.17.5:1688", provider => 'powershell')
+
+    end
+
+    it "should execute 'Activate Windows Part 2'" do
+
+      is_expected.to contain_exec('Activate Windows Part 2').with('command' => "slmgr.vbs /ato", provider => 'powershell')
+
+    end
+
+    it "should execute 'Activate Windows Part 3'" do
+
+      is_expected.to contain_exec('Activate Windows Part 2').with('command' => "slmgr.vbs /dli", provider => 'powershell')
+
+    end
+
+  end
+
+  describe 'Office Activation Executions' do
+
+    it "should execute 'Activate Office Part 1'" do
+
+      is_expected.to contain_exec('Activate Office Part 1').with('command' => "cscript 'C:\\Program Files (x86)\\Microsoft Office\\Office15\\ospp.vbs' /remhst", provider => 'powershell')
+      
+    end
+
+    it "should execute 'Activate Office Part 2'" do
+
+      is_expected.to contain_exec('Activate Office Part 2').with('command' => "cscript 'C:\\Program Files (x86)\\Microsoft Office\\Office15\\ospp.vbs' /sethst:10.129.17.5", provider => 'powershell')
+
+    end
+
+    it "should execute 'Activate Office Part 3'" do
+
+      is_expected.to contain_exec('Activate Office Part 3').with('command' => "cscript 'C:\\Program Files (x86)\\Microsoft Office\\Office15\\ospp.vbs' /act", provider => 'powershell')
+
+    end
+
+    it "should execute 'Activate Office Part 4'" do
+
+      is_expected.to contain_exec('Activate Office Part 4').with('command' => "cscript 'C:\\Program Files (x86)\\Microsoft Office\\Office15\\ospp.vbs' /remhst", provider => 'powershell')
+
+    end
+
+    it "should execute 'Activate Office Part 5'" do
+
+      is_expected.to contain_exec('Activate Office Part 5').with('command' => "cscript 'C:\\Program Files (x86)\\Microsoft Office\\Office15\\ospp.vbs' /dstatus", provider => 'powershell')
 
     end
 
@@ -27,15 +80,15 @@ describe 'ngc_galaxy' do
 
   describe 'resources' do
 
-    it 'should have one resource' do
+    it 'should have nine resources' do
 
-      is_expected.to have_resource_count(1)
+      is_expected.to have_resource_count(8)
 
     end
 
-    it 'should have one exec resource' do
+	  it 'should have nine exec resource' do
 
-      is_expected.to have_exec_resource_count(1)
+	    is_expected.to have_exec_resource_count(8)
 
     end
 
