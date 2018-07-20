c839d71340df38704e8677c0552cf7b861e3228f
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-20 09:54:20 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -3,7 +3,7 @@ class ngc_wsus {
   exec{"Execute batch file":
     
     command      => '& C:\opt\wsus.ps1',
-    onlyif       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ /f WUServer) -like "WUServer" -split "\s+",3)[2] -ne "http://10.129.17.5"){ Exit 1 }',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ /f WUServer) -like "WUServer" -split "\s+",3)[2] -ne "http://10.129.17.5"){ Exit 1 }',
     provider     => powershell,
     returns      => 0,
     logoutput    => true,
