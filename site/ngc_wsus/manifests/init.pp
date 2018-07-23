9b0609cf3a7552cc36bb843b313a3905a4d3b71e
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-23 09:56:42 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -19,8 +19,7 @@ class ngc_wsus {
   }
     
   exec{"Execute NoAutoUpdate Configuration":
-    
-    command      => '& C:\opt\wsus.ps1',
+        command      => '& C:\opt\wsus.ps1',
     unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
     provider     => powershell,
     returns      => 0,
@@ -31,6 +30,60 @@ class ngc_wsus {
     
     command      => '& C:\opt\wsus.ps1',
     unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AUOptions) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute DetectionFrequency Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v DetectionFrequency) -split "\s+",5)[5] -ne "0x16"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute DetectionFrequencyEnabled Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f DetectionFrequencyEnabled) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute NoAutoRebootWithLoggedOnUsers Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoRebootWithLoggedOnUsers) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute NoAutoUpdate Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute ScheduledInstallDay Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallDay) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+  }
+  
+  exec{"Execute ScheduledInstallTime Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallTime) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
     provider     => powershell,
     returns      => 0,
     logoutput    => true,
