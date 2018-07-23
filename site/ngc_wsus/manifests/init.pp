4d85637c250fa235b2b50450afa52d1491da2fe9
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-23 09:24:56 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -16,5 +16,21 @@ class ngc_wsus {
     provider     => powershell,
     returns      => 0,
     logoutput    => true,
+    
+  exec{"Execute NoAutoUpdate Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
+    
+  exec{"Execute AUOptions Configuration":
+    
+    command      => '& C:\opt\wsus.ps1',
+    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AUOptions) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
+    provider     => powershell,
+    returns      => 0,
+    logoutput    => true,
 
 }
