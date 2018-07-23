e4d5540944dcaef3703f9d0037f32550054db339
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-23 10:26:18 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -61,16 +61,7 @@ class ngc_wsus {
     returns      => 0,
     logoutput    => true,
   }
-  
-  exec{"Execute NoAutoUpdate Configuration":
     
-    command      => '& C:\opt\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-  
   exec{"Execute ScheduledInstallDay Configuration":
     
     command      => '& C:\opt\wsus.ps1',
