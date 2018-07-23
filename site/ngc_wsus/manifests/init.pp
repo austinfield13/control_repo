7a2ea0cab6dd14700aa0c2d0a28ee43cab463a75
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-23 10:23:12 -0600

    Update init.pp

--- site/ngc_wsus/manifests/init.pp
+++ site/ngc_wsus/manifests/init.pp
@@ -19,7 +19,7 @@ class ngc_wsus {
   }
     
   exec{"Execute NoAutoUpdate Configuration":
-        command      => '& C:\opt\wsus.ps1',
+    command      => '& C:\opt\wsus.ps1',
     unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
     provider     => powershell,
     returns      => 0,
