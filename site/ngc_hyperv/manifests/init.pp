e9a6fe3c3754ba8d4a5b02b9df734856c183291b
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-27 10:46:54 -0600

    Update init.pp

--- site/ngc_hyperv/manifests/init.pp
+++ site/ngc_hyperv/manifests/init.pp
@@ -1,12 +1,12 @@
 class ngc_hyperv {
 
   exec {'hyperv':
-  
-    command     => 'DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V',
+
+    command     => '& "S:\\hyper-v\\Enable-HyperV.ps1"',
     unless      => 'if((Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State -ne "Enabled"){ Exit 1 }else{ Exit 0}',
     provider    => 'powershell',
     logoutput   => true,
     returns     => 0,
-  }
 
+  }
 }
