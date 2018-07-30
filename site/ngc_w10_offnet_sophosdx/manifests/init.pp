441ade64649aa77baf3a0376c206c608cf05ea3a
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-30 12:26:47 -0600

    Update init.pp

--- site/ngc_w10_offnet_sophosdx/manifests/init.pp
+++ site/ngc_w10_offnet_sophosdx/manifests/init.pp
@@ -9,7 +9,7 @@ class ngc_w10_offnet_sophosdx {
   exec {'SophosDXOffNet':
 
     subscribe        => File['C:\opt\W10-OffNet-SophosDX'],
-    command          => 'Start-Process S:\W10-OffNet-SophosDX\SOPHOSDXOffNet.BAT'},
+    command          => 'Start-Process S:\W10-OffNet-SophosDX\SOPHOSDXOffNet.BAT',
     provider         => powershell,
     refreshonly      => true,
     
