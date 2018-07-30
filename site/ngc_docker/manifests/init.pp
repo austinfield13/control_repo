fedd6f834e9b020c10dc6068837ad4fff06fd949
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-30 08:51:38 -0600

    Update init.pp

--- site/ngc_docker/manifests/init.pp
+++ site/ngc_docker/manifests/init.pp
@@ -10,7 +10,7 @@ class ngc_docker {
 
     ensure            => installed,
     source            => 'S:\docker\Docker for Windows Installer.exe',
-    install_options   => [{'/DIR' => 'C:\opt\docker'}],
+    install_options   => ['/S'],
 	
   }
 }
