9d566c04351ca0f078f612feb0ae4ffe961b192a
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-07-31 15:29:41 -0600

    Update init.pp

--- site/ngc_disable_notification_center/manifests/init.pp
+++ site/ngc_disable_notification_center/manifests/init.pp
@@ -12,7 +12,7 @@ class ngc_disable_notification_center {
   exec{"Disable Notifications":
     
     command      => 'regedit /s S:\windows-10\DisableNotificationCenter.reg',
-    unless       => 'if((REG QUERY HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer /f DisableNotificationCenter) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    unless       => 'if(((REG QUERY HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer /f DisableNotificationCenter) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
     provider     => powershell,
     returns      => 0,
     logoutput    => true,
