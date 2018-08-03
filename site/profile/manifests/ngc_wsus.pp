638face930d196269229d14785d94eeab0f3e682
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-03 09:39:31 -0600

    Update ngc_wsus.pp

--- site/profile/manifests/ngc_wsus.pp
+++ site/profile/manifests/ngc_wsus.pp
@@ -3,119 +3,165 @@
 #
 #
 # @example
-#   include profile::ngc_selenium
+#   include profile::ngc_wsus
 class profile::ngc_wsus {
 
   registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\WUServer':
   
     ensure       => present,
-    type         => string,
-    data         => 'http://10.129.17.5',
+	type         => string,
+	data         => 'http://10.129.17.5',
   }
 
   registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\WUStatusServer':
   
     ensure       => present,
-    type         => string,
-    data         => 'http://10.129.17.5',
+	type         => string,
+	data         => 'http://10.129.17.5',
   }
 
-  #exec{"Execute WUServer Configuration":
-    
-    #command      => '& S:\galaxy\wsus\wsus.ps1',
-    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ /f WUServer) -split "\s+",5)[5] -ne "http://10.129.17.5"){ Exit 1 }else{ Exit 0 }',
-    #provider     => powershell,
-    #returns      => 0,
-    #logoutput    => true,  
-  #}
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoUpdate':
   
-  #exec{"Execute WUStatusServer Configuration":
-    
+    ensure       => present,
+	type         => dword,
+	data         => '0',
+  }
+
+  #exec{"Execute NoAutoUpdate Configuration":
     #command      => '& S:\galaxy\wsus\wsus.ps1',
-    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ /f WUStatusServer) -split "\s+",5)[5] -ne "http://10.129.17.5"){ Exit 1 }else{ Exit 0 }',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
     #provider     => powershell,
     #returns      => 0,
     #logoutput    => true,
   #}
-    
-  exec{"Execute NoAutoUpdate Configuration":
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoUpdate) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-    
-  exec{"Execute AUOptions Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AUOptions) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\AUOptions':
   
-  exec{"Execute DetectionFrequency Configuration":
+    ensure       => present,
+	type         => dword,
+	data         => '3',
+  }  
     
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v DetectionFrequency) -split "\s+",5)[5] -ne "0x16"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
+  #exec{"Execute AUOptions Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AUOptions) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
   
-  exec{"Execute DetectionFrequencyEnabled Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f DetectionFrequencyEnabled) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\DetectionFrequency':
   
-  exec{"Execute NoAutoRebootWithLoggedOnUsers Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoRebootWithLoggedOnUsers) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-    
-  exec{"Execute ScheduledInstallDay Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallDay) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-  
-  exec{"Execute ScheduledInstallTime Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallTime) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-  
-  exec{"Execute AutoInstallMinorUpdates Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AutoInstallMinorUpdates) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
-  }
-  
-  exec{"Execute UseWUServer Configuration":
-    
-    command      => '& S:\galaxy\wsus\wsus.ps1',
-    unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f UseWUServer) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
-    provider     => powershell,
-    returns      => 0,
-    logoutput    => true,
+    ensure       => present,
+	type         => dword,
+	data         => '16',
   }
 
+  #exec{"Execute DetectionFrequency Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v DetectionFrequency) -split "\s+",5)[5] -ne "0x16"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\DetectionFrequencyEnabled':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '1',
+  }
+  
+  #exec{"Execute DetectionFrequencyEnabled Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f DetectionFrequencyEnabled) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoRebootWithLoggedOnUsers':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '1',
+  }
+  
+  #exec{"Execute NoAutoRebootWithLoggedOnUsers Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f NoAutoRebootWithLoggedOnUsers) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\ScheduledInstallDay':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '0',
+  }
+    
+  #exec{"Execute ScheduledInstallDay Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallDay) -split "\s+",5)[5] -ne "0x0"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\ScheduledInstallTime':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '3',
+  }
+  
+  #exec{"Execute ScheduledInstallTime Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f ScheduledInstallTime) -split "\s+",5)[5] -ne "0x3"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\AutoInstallMinorUpdates':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '1',
+  }
+  
+  #exec{"Execute AutoInstallMinorUpdates Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f AutoInstallMinorUpdates) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\UseWUServer':
+  
+    ensure       => present,
+	type         => dword,
+	data         => '1',
+  }
+  
+  #exec{"Execute UseWUServer Configuration":
+    
+    #command      => '& S:\galaxy\wsus\wsus.ps1',
+    #unless       => 'if(((REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /f UseWUServer) -split "\s+",5)[5] -ne "0x1"){ Exit 1 }else{ Exit 0 }',
+    #provider     => powershell,
+    #returns      => 0,
+    #logoutput    => true,
+  #}
+
 }
+
