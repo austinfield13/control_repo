e2954e5913c4f2a6f18f592cb7135de90d413830
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-06 15:05:41 -0600

    Update init.pp

--- site/puppetboard/manifests/init.pp
+++ site/puppetboard/manifests/init.pp
@@ -2,11 +2,13 @@ class puppetboard {
   class { 'apache': }
   class { 'apache::mod::wsgi': }
   class { 'puppetboard':
-    report_count  => 40,  
+    report_count       => 40,
+    manage_git         => true,
+    manage_virtualenv  => true,
   }
   class { 'puppetboard::apache::vhost':
-    vhost_name     => 'pboard.example.com',
-    port           => 80,
+    vhost_name         => 'mizar.entds.ngisn.com',
+    port               => 80,
   }
 }
 
