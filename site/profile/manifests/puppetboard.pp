db93bd298b24df0ebab9a50e74fc130118329d10
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-06 15:08:58 -0600

    Create puppetboard.pp

---
+++ site/profile/manifests/puppetboard.pp
@@ -0,0 +1,21 @@
+class profile::puppetboard {
+
+  class { 'apache': }
+
+  class { 'apache::mod::wsgi': }
+
+  class { 'puppetboard':
+
+    report_count       => 40,
+    manage_git         => true,
+    manage_virtualenv  => true,
+  }
+
+  class { 'puppetboard::apache::vhost':
+
+    vhost_name         => 'mizar.entds.ngisn.com',
+    port               => 80,
+
+  }
+
+}
