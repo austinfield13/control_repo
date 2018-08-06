87ac9e1f110b9ad7b014b3b54baecd57ce2b874c
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-06 14:49:30 -0600

    Update puppetboard.pp

--- site/puppetboard/manifests/puppetboard.pp
+++ site/puppetboard/manifests/puppetboard.pp
@@ -1,9 +1,11 @@
-class { 'apache': }
-class { 'apache::mod::wsgi': }
-class { 'puppetboard':
-  report_count  => 40,
-}
-class { 'puppetboard::apache::vhost':
- vhost_name     => 'pboard.example.com',
- port           => 80,
+class {'puppetboard':
+  class { 'apache': 
+  class { 'apache::mod::wsgi': }
+  class { 'puppetboard':
+    report_count  => 40,
+  }
+  class { 'puppetboard::apache::vhost':
+   vhost_name     => 'pboard.example.com',
+   port           => 80,
+  }
 }
