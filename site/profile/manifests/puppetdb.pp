32c5d1fc8b02299054873bc70c0f36e4915ffdd4
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-07 14:12:50 -0600

    Update puppetdb.pp

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -1,6 +1,8 @@
 class profile::puppetdb {
   # Configure puppetdb and its underlying database
-  class { 'puppetdb': }
+  class { 'puppetdb':
+    listen_address     => 'mizar.entds.ngisn.com',
+    listen_port        => '80',}
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
 }
