cbe1eee0cdefbe22ca5d46364e180a243855859d
austinfield13 <40214574+austinfield13@users.noreply.github.com>
2018-08-09 19:54:50 +0000

    Update ngc_hyperv_users.pp

--- site/profile/manifests/ngc_hyperv_users.pp
+++ site/profile/manifests/ngc_hyperv_users.pp
@@ -3,9 +3,9 @@ class profile::ngc_hyperv_users {
   exec {'Add ENTDS group to Hyper-V administrative users':
 
     command    => '& S:\hyper-v\Add-Hyperv-Group.ps1',
-	  unless     => 'if((net localgroup "Hyper-V Administrators") -match "ENTDS\\P3274-NSDC_PLN-Users"){Exit 0}else{Exit 1}',
-	  provider   => powershell,
-	  logoutput  => true,
-	  returns    => 0,
+    unless     => 'if((net localgroup "Hyper-V Administrators") -match "ENTDS\\P3274-NSDC_PLN-Users"){Exit 0}else{Exit 1}',
+    provider   => powershell,
+    logoutput  => true,
+    returns    => 0,
   }
 }
